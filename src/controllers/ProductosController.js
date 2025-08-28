b36a58a01ac5a48e4330069b7c6241339c8eda43
EnriqueSazoR <luisrosales9598@gmail.com>
2025-08-27 22:05:51 -0600

    Implementación de metodos GET - PUT para productos

--- src/controllers/ProductosController.js
+++ src/controllers/ProductosController.js
@@ -6,16 +6,24 @@ const prisma = new PrismaClient();
 export const PostProducto = async (req, res) => {
   try {
     const { nombre, stock, categoria, marca } = req.body;
+
+    // validar que el producto no existe
+    const productoExistente = await prisma.Producto.findFirst({
+      where : {nombre}
+    })
+    if(productoExistente) {
+      return res.status(400).json({error : `EL producto ${nombre} ya existe`})
+    }
     // Buscar id de categoria
     const categoriaObj = await prisma.Categoria.findFirst({
-      where: { nombre : categoria.trim()},
+      where: { nombre: categoria.trim() },
     });
     if (!categoriaObj) {
       return res.status(404).json({ error: "Categoría no encontrada" });
     }
     // Buscar id de marca
     const marcaObj = await prisma.Marca.findFirst({
-      where: { nombre : marca.trim()},
+      where: { nombre: marca.trim() },
     });
     if (!marcaObj) {
       return res.status(404).json({ error: "Marca no encontrada" });
@@ -26,15 +34,110 @@ export const PostProducto = async (req, res) => {
       data: {
         nombre,
         stock,
-        categoria : {
-            connect : {id : categoriaObj.id}
+        categoria: {
+          connect: { id: categoriaObj.id },
         },
-        marca : {
-            connect: {id: marcaObj.id}
-        }
+        marca: {
+          connect: { id: marcaObj.id },
+        },
       },
     });
     res.status(200).json({ mensaje: "Producto creado" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Error en servidor" });
+  }
+};
+
+// Método para mostrar todas las categorias
+export const GetProdutcos = async (req, res) => {
+  try {
+    const listaProductos = await prisma.Producto.findMany({
+      select: {
+        nombre: true,
+        categoria: {
+          select: {
+            nombre: true,
+          },
+        },
+        marca: {
+          select: {
+            nombre: true,
+          },
+        },
+      },
+    });
+    res.send({
+      Productos: listaProductos,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Error en el servidor" });
+  }
+};
+
+export const PutProducto = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { nombre, stock, categoria, marca } = req.body;
+
+    // Buscar objeto por ID
+    const buscarProducto = await prisma.Producto.findUnique({
+      where: { id: parseInt(id) },
+    });
+
+    if (!buscarProducto) {
+      res.status(404).json({ error: "Producto no encontrado" });
+    }
+
+    // Validar si la categoria ingresada existe
+    const categoriaObj = await prisma.Categoria.findFirst({
+      where : {nombre : categoria.trim()}
+    })
+    if(!categoriaObj) {
+      return res.status(404).json({error : "La categoria no existe"})
+    }
+
+    const marcaObj = await prisma.Marca.findFirst({
+      where : {nombre : marca.trim()}
+    })
+    if(!marcaObj) {
+      return res.status(404).json({error : "La marca no existe"})
+    }
+
+    // Actualizar Producto
+    const productoActualizado = await prisma.Producto.update({
+      where: { id: parseInt(id) },
+      data: {
+        nombre: nombre ?? producto.nombre,
+        stock: stock ?? producto.stock,
+        categoria : {
+          connect : {id : categoriaObj.id}
+        },
+        marca : {
+          connect : {id : marcaObj.id}
+        }
+      },
+      select: {
+        nombre: true,
+        stock: true,
+        categoria : {
+          select : {
+            nombre : true
+          }
+        },
+        marca : {
+          select : {
+            nombre : true
+          }
+        },
+        fechaActualizacion: true,
+      },
+    });
+    res.json({
+      exito: "Producto Actualizado",
+      producto: productoActualizado,
+    });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Error en servidor" });
--- src/routes/ProductoRoutes.js
+++ src/routes/ProductoRoutes.js
@@ -1,11 +1,13 @@
 // Rutas para productos
 import { Router } from "express";
-import { PostProducto } from "../controllers/ProductosController.js";
+import { PostProducto, GetProdutcos, PutProducto } from "../controllers/ProductosController.js";
 import { validarDatos } from "../middlewares/middlewareProducto.js";
 const router = Router()
 
 
 router.post('/crear', validarDatos, PostProducto)
+router.get('/lista', GetProdutcos)
+router.put('/actualizar/:id', validarDatos, PutProducto)
 
 
 export default router
